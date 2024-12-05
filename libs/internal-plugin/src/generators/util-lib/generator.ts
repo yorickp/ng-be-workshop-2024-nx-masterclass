5b3145aec7770ba16238229fd10df802fd56a308
yorickp <yorick.phlypo@corilus.be>
2024-12-05 09:57:19 +0100

    chore(nx): custom-plugins > 9

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -6,19 +6,36 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
+import { libraryGenerator } from '@nx/js';
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
+  // 6. Console log the properties
+  console.log('Name specified:', options.name);
+
+  // 7. Add prefix
+  const libName = options.name.startsWith('util-')
+    ? options.name
+    : 'util-' + options.name;
+
+  // 5. Extend other generators
+  /* const projectRoot = `libs/${options.name}`;
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
     projectType: 'library',
     sourceRoot: `${projectRoot}/src`,
     targets: {},
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options); */
+
+  await libraryGenerator(tree, {
+    directory: 'libs/' + options.directory + '/' + libName,
+    // 9. Add tags
+    tags: 'scope:' + options.directory,
+  });
+
   await formatFiles(tree);
 }
 
--- libs/internal-plugin/src/generators/util-lib/schema.d.ts
+++ libs/internal-plugin/src/generators/util-lib/schema.d.ts
@@ -1,3 +1,4 @@
 export interface UtilLibGeneratorSchema {
   name: string;
+  directory: string;
 }
